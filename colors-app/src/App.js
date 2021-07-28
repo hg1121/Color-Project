d3cc30bbc2c6381c28dc9151707fec94157817f8
Hong Guo <hg@Hongs-MacBook-Air.local>
2021-07-27 17:49:30 -0700

    Style ColorBox

--- colors-app/src/App.js
+++ colors-app/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       {/* ...makes the props not an array but an object */}
-      <Palette {...seedColors[1]}/>
+      <Palette {...seedColors[4]}/>
     </div>
   );
 }
