ee09cd58091410efa57b178bbf2c151c198c2ce6
Hong Guo <hg@Hongs-MacBook-Air.local>
2021-07-28 17:16:28 -0700

    Add Router PaletteList and Link

--- colors-app/src/App.js
+++ colors-app/src/App.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
 import seedColors from'./seedColors.js';
 import { generatePalette } from './colorHelpers';
+import PaletteList from './PaletteList';
 
 class App extends Component {
+  findPalette(id){
+    return seedColors.find( function(palette) {
+        return palette.id === id;
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        {/* ...makes the props not an array but an object */}
-        <Palette palette = { generatePalette(seedColors[4]) }/>
-      </div>
+      <Switch>
+          <Route 
+            exact path='/' 
+            render={ ()=><PaletteList palettes = {seedColors}/> }>
+
+          </Route>
+          <Route 
+            exact path='/palette/:id' 
+            render={ routeProps => 
+                <Palette palette = { generatePalette(this.findPalette(routeProps.match.params.id)) }/> }
+          >
+          </Route>
+          {/* <div className="App">
+            <Palette palette = { generatePalette(seedColors[4]) }/>
+          </div>  */}
+      </Switch>
     );
   }
 }
